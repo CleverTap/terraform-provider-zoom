c12952025061d018fa38bd05963fb3518da54a69
Namrata Chaurasia <50015194+chaurasia-namrata@users.noreply.github.com>
2021-06-21 17:25:01 +0530

    removed validation for white space in name

--- zoom/resource_user.go
+++ zoom/resource_user.go
@@ -21,11 +21,6 @@ func validateName(v interface{}, k string) (ws []string, es []error) {
 		errs = append(errs, fmt.Errorf("Expected name to be string"))
 		return warns, errs
 	}
-	whiteSpace := regexp.MustCompile(`\s+`)
-	if whiteSpace.Match([]byte(value)) {
-		errs = append(errs, fmt.Errorf("name cannot contain whitespace. Got %s", value))
-		return warns, errs
-	}
 	nameRegex := regexp.MustCompile("^[A-Za-z]\\w{5,29}$")
 
 	if !(nameRegex.MatchString(k)) {
