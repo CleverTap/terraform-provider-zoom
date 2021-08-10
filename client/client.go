3760b45b845504d1bfcc4dbc677adce42c4c5832
shubham <shubham@clevertap.com>
2021-08-10 13:40:50 +0530

    replce dissociate operation with delete operation

--- client/client.go
+++ client/client.go
@@ -46,6 +46,10 @@ type UpdateUser struct {
 	Location    string `json:"location"`
 }
 
+type DeleteUser struct {
+	Action string `json:"action"`
+}
+
 var (
     Errors = make(map[int]string)
 )
@@ -235,7 +239,12 @@ func (c *Client) updatehttpRequest(path,method string, body bytes.Buffer, item *
 }
 
 func (c *Client) DeleteItem(userId string) error {
-	_, err := c.deletehttpRequest(fmt.Sprintf("%s", userId), "DELETE", bytes.Buffer{})
+	userjson := DeleteUser{
+		Action: "delete",
+	}
+	reqjson, _ := json.Marshal(userjson)
+	payload := strings.NewReader(string(reqjson))
+	_, err := c.deletehttpRequest(fmt.Sprintf("%s", userId), "DELETE", payload)
 	if err != nil {
 		log.Println("[DELETE ERROR]: ", err)
 		return err
@@ -243,8 +252,8 @@ func (c *Client) DeleteItem(userId string) error {
 	return nil
 }
 
-func (c *Client) deletehttpRequest(path, method string, body bytes.Buffer) (closer io.ReadCloser, err error) {
-	req, err := http.NewRequest(method,fmt.Sprintf("%s/%s", "https://api.zoom.us/v2/users", path + "?action=\"delete\""), &body)
+func (c *Client) deletehttpRequest(path, method string, body *strings.Reader) (closer io.ReadCloser, err error) {
+	req, err := http.NewRequest(method,fmt.Sprintf("%s/%s", "https://api.zoom.us/v2/users", path), body)
 	if err != nil {
 		log.Println("[DELETE ERROR]: ", err)
 		return nil, err
